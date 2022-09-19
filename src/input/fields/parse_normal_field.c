5508f249f86ac90af4219d234354fa3df949d796
Mariusmivw <mvan-wij@student.codam.nl>
2022-09-19 12:36:16 +0200

    Normal fields more lax

--- src/input/fields/parse_normal_field.c
+++ src/input/fields/parse_normal_field.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/27 15:05:33 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/09/19 11:44:02 by mvan-wij      ########   odam.nl         */
+/*   Updated: 2022/09/19 12:34:56 by mvan-wij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 #include "utils/utils.h"
 #include "vec3/vec3.h"
 #include <stdlib.h>
+#include <math.h>
 
 t_status	parse_normal_field(char **line, t_vec3 *vec)
 {
 	if (parse_vec_field(line, vec) != OK)
 		return (FAIL);
-	if (!almost_equal(mag2(*vec), 1))
+	if (fabsl(mag2(*vec) - 1) > 0.001)
 		return (rt_set_error(E_EXPECTED_NORMAL, NULL));
+	*vec = normalize(*vec);
 	return (OK);
 }
